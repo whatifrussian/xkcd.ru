c02fcaa1554e927fdeff8b8d803d85154c67ca5b
django <django@iakov.davydov.name>
2008-09-14 06:53:53 -0400

    add admin; fix base.html

---
+++ comics/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from comics.models import Comics
+
+class ComicsAdmin(admin.ModelAdmin):
+        list_display = ('cid', 'title', 'image_preview', 'visible')
+        ordering = ('visible', )
+        list_per_page = 10
+
+
+admin.site.register(Comics, ComicsAdmin)
